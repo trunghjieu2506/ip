444011089a25211ad9e5041758ba78ad2996104a
trunghjieu2506 <>
2025-01-30 20:43:24 +0800

    Level-2

--- src/main/java/Corgi.java
+++ src/main/java/Corgi.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Corgi {
-    public static String partition = "************";
-    public static void main(String[] args) {
+    public static String partition = "************************";
+    public static String indent = "     ";
 
+    public static ArrayList<String> text_storage = new ArrayList<>();
+
+    public static void main(String[] args) {
 
         System.out.println("Hello! I'm Corgi");
         System.out.println("What can I do for you?");
@@ -18,15 +22,27 @@ public class Corgi {
                 corgi_print("Bye. Hope to see you again soon!");
                 break;
             }
-            corgi_print(input);
+            else if (input.equalsIgnoreCase("list")) {
+                show_text_storage();
+            }
+            else {
+                text_storage.add(input);
+                corgi_print("added: " + input);
+            }
         }
     }
 
     public static void corgi_print(String input) {
-        String indent = "     ";
-        System.out.println(partition);
+        System.out.println(indent + partition);
         System.out.println(indent + input);
-        System.out.println(partition);
+        System.out.println(indent + partition);
+    }
 
+    public static void show_text_storage() {
+        System.out.println(indent + partition);
+        for (int i = 0; i < text_storage.size(); i++) {
+            System.out.println(indent + (i + 1) + "." + text_storage.get(i));
+        }
+        System.out.println(indent + partition);
     }
 }
