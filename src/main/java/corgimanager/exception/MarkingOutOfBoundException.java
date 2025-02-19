d2eee88a5b30607ba764633270c6df431a3fe967
trunghjieu2506 <trunghieu.itw@gmail.com>
2025-02-19 13:08:16 +0800

    delete redundant exception class MarkingOutOfBoundException.java

--- src/main/java/corgimanager/exception/MarkingOutOfBoundException.java
+++
@@ -1,7 +0,0 @@
-package corgimanager.exception;
-
-public class MarkingOutOfBoundException extends Exception {
-    public MarkingOutOfBoundException(String message) {
-        super(message);
-    }
-}
